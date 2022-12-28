1d61d50ee0bc91c87ac82cdf059c4437756f1494
James Whitby <jameswhitby813@gmail.com>
2022-12-28 13:26:16 +0000

    settings for railway

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nxv+!$l7e+(uvubsm^()^n9#dhi0q!x0^jtc(%h_^-y86-fe*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
